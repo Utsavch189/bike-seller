9b839ba41dfc8fc50842dc098a03811cc1f3d4b5
Utsav <utsavpokemon9000chatterjee@gmail.com>
2023-12-21 15:42:14 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './components/main/Navbar';
+import Products from './pages/Products';
+import SpeceficProduct from './pages/SpeceficProduct';
+
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Products/>
+      
+    </>
+  );
+}
+
+export default App;
