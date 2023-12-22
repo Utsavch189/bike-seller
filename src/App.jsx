1d96a077ecdbec5ea982fc95600f88417437632a
supratim531 <91423964+supratim531@users.noreply.github.com>
2023-12-22 11:04:52 +0530

    Added Footer & MyCarousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import MyCarousel from './components/main/MyCarousel';
+import Footer from './components/main/Footer';
 import Navbar from './components/main/Navbar';
-
 
 function App() {
   return (
     <>
       <Navbar />
+      {/* <MyCarousel /> */}
       <Outlet />
+      <Footer />
     </>
   );
 }
