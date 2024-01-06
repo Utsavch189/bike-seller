0c1501e2a69659d71b1aa716c149b365232d30cb
Utsav <utsavpokemon9000chatterjee@gmail.com>
2024-01-06 14:21:15 +0530

    add lottie animation

--- src/components/main/Loader.jsx
+++ src/components/main/Loader.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import animation from '../../assets/summer-trip.json'
 
 function Loader() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
   return (
     <>
-      <div className="flex space-x-2 justify-center items-center h-[70vh] bg-white">
+    <Lottie options={defaultOptions}
+              height={400}
+              width={400}/>
+      {/*<div className="flex space-x-2 justify-center items-center h-[70vh] bg-white">
         <span className="sr-only">Loading...</span>
         <div className="h-8 w-8 bg-blue-600 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
         <div className="h-8 w-8 bg-blue-600 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
         <div className="h-8 w-8 bg-blue-600 rounded-full animate-bounce"></div>
-      </div>
+  </div>*/}
     </>
   )
 }
